b36c53594c90259ec38e9db6c4b6f9366993a7ab
awelvor <philippe.regent@gmail.com>
2025-08-18 11:48:03 +0200

    Update url.js

--- js/url.js
+++ js/url.js
@@ -90,6 +90,11 @@ const jpg2025 = [
 {url: "PXL_20250424_155313996.PORTRAIT~2.jpg"},
 {url: "marielle_20250115.jpg"},
 {url: "marielle_20250116.jpg"},
+{url: "IMG-20250818-WA0000.jpg"},
+{url  "IMG-20250818-WA0001.jpg"},
+{url: "IMG-20250818-WA0002.jpg"},
+{url: "IMG-20250818-WA0003.jpg"},
+{url: "IMG-20250818-WA0004.jpg"},
 ];
 
 
